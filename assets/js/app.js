9c005da3d5219e61ad198f244078cbfda19382ec
bartek2397 <bartosz.wszolek97@gmail.com>
2022-03-30 13:25:37 +0200

    .

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+let bill = document.querySelector('#bill')
+let tip = document.querySelector('.tip')
+let custom = document.querySelector('#custom')
+let numberOfPeople = document.querySelector('#numberOfPeople')
+let tipResult = document.querySelector('.tipResult')
+let tip5 = document.querySelector('.tip5')
+let tip10 = document.querySelector('.tip15')
+let tip15 = document.querySelector('.tip15')
+let tip25 = document.querySelector('.tip25')
+let tip50 = document.querySelector('.tip50')
+
+let getBill = () => {
+    bill.value
+}
+
+tip5.addEventListener('click', () => {
+    
+})
+
