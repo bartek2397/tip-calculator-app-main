831159fcc779ccd816886574357fbc54be667a1d
bartek2397 <bartosz.wszolek97@gmail.com>
2022-04-02 12:58:28 +0200

    added some JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
 const bill = document.querySelector('#bill')
 const custom = document.querySelector('#custom')
 const numberOfPeople = document.querySelector('#numberOfPeople')
-const tipResult = document.querySelector('.tipResult')
-const totalResult = document.querySelector('.totalResult')
+const tipResult = document.querySelector('.amountNum')
+const totalResult = document.querySelector('.totalNum')
+const errorText = document.querySelector('.errorText')
 const tip5 = document.querySelector('.tip5')
 const tip10 = document.querySelector('.tip10')
 const tip15 = document.querySelector('.tip15')
@@ -14,9 +15,14 @@ const btnList = [tip5, tip10, tip15, tip25, tip50]
 let isCustom = false
 let tipDefault = `0.00`
 let totalDefault = `0.00`
+let customTip = '';
+let billAmount = bill.value;
+let guests = numberOfPeople.value;
 
 tipResult.innerText = tipDefault
 totalResult.innerText = totalDefault
+tip5.classList.add("active");
+setTip(5);
 
 tip5.addEventListener('click', () => {
     if (tip5.classList.contains('active')) {
@@ -73,12 +79,66 @@ tip50.addEventListener('click', () => {
         tip50.classList.toggle('active')
     }
 })
-getBill()
+document.addEventListener('change', () => {
+    if (isCustom === true) {
+        tip5.classList.remove('active')
+        tip10.classList.remove('active')
+        tip15.classList.remove('active')
+        tip25.classList.remove('active')
+        tip50.classList.remove('active')
+    }
+})
 
-function tipAmount() {
-    if (bill.value == 0) {
-        return
+bill.addEventListener('change', () => {
+    showSum()
+})
+
+function getBill() {
+    //get the value
+    billAmount = bill.value;
+  
+    // when user inputs decimal, only allow 2 characters after it.
+    if (billAmount.includes(".")) {
+      i = billAmount.indexOf(".");
+      x = i + 3;
+      billAmount = billAmount.slice(0, x);
+  
+      //set display and amount
+      bill.value = billAmount;
+  
+      showSum();
+      return;
+    }
+  }
+
+function showSum() {
+    x = (billAmount * tipAmount) / 100
+    i = guests
+    splitTip = x / i
+    billTotal = billAmount / i + splitTip
+
+    tipDefault.innerText = splitTip.toFixed(2)
+    totalDefault.innerText = billTotal.toFixed(2)
+    
+}
+
+function setTip (number) {
+    if (isCustom === true) {
+        tipAmount = customTip;
     } else {
-        bill.value 
+        tipAmount = number
+    }
+    showSum()
+}
+
+let error = () => {
+    if (billAmount === 'string') {
+        errorText.innerText = 'The bill should be a number!'
+    } else if (billAmount > 3000) {
+        errorText.innerText = 'The bill is too large!'
+    } else if (billAmount < 0) {
+        errorText.innerText = 'The bill must be above 0!'
+    } else {
+        showSum()
     }
 }
